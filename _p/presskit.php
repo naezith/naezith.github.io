2bd5b77c8029e6ff9b46f97d70b4187eebabe1d1
naezith <tolgaay@live.com>
2017-09-10 16:31:39 +0300

    init

---
+++ _p/presskit.php
@@ -0,0 +1,314 @@
+<?php include_once("analyticstracking.php") ?>
+
+<div class="presskit">
+				<div class="left">
+					<h2>Presskit</h2>
+					<ul class="kit_nav">
+						<li><a href="#facts">Facts</a></li>
+						<li><a href="#overview">Overview</a></li>
+						<li><a href="#history">History</a></li>
+						<li><a href="#features">Features</a></li>
+						<li><a href="#videos">Videos</a></li>
+						<li><a href="#images">Images</a></li>
+						<li><a href="#logo">Logo & Icon</a></li>
+						<li><a href="#credits">Credits</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+					
+					<div class="kit_facts">
+						<h3 id="facts">Facts</h3>
+						<ul class="nested">
+							<li>Developer
+								<ul>
+									<li>Tolga Ay</li>
+									<li>Istanbul, Turkey</li>
+								</ul>
+							</li>
+							
+							<li>Release date
+								<ul>
+									<li>2017</li>
+								</ul>
+							</li>
+							
+							<li>Platforms
+								<ul>
+									<li>Windows</li>
+									<li>Mac</li>
+									<li>Linux</li>
+								</ul>
+							</li>
+							
+							<li>Engine used
+								<ul>
+									<li>Custom Engine with C++ and SFML</li>
+								</ul>
+							</li>
+							
+							<li>Project started
+								<ul>
+									<li>04.08.2014</li>
+								</ul>
+							</li>
+							
+							<li>Website
+								<ul>
+									<li><a href="http://naezith.com">naezith.com</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				
+				</div>
+				
+				<div class="kit_descr">
+					<figure class="iframe steam">
+						<iframe src="http://store.steampowered.com/widget/590590/"></iframe>
+					</figure>
+					
+					<p><a class="center extlink" href="https://www.dropbox.com/sh/kb2i1ve1aim4v5m/AACRGwbARNDirDsaVzcsiwE0a?dl=0&lst=">Browse all the presskit assets on Dropbox</a></p>
+					
+					<h3 id="overview">Overview</h3>
+					<p>Feel the acceleration in this fast-paced grappling hook platformer, Remnants of Naezith.</p>
+
+					<p>Remnants of Naezith is a fast-paced precision platformer oriented around grappling hook mechanics. 
+					Rush through levels and swing around obstacles, improve your skills to climb the leaderboards, or even create your own levels. 
+					The fun of swinging with grappling hooks and the amazing feel of acceleration makes a great combination during the gameplay.</p>
+
+					<p>You play as Kayra, a human who shares his body with the soul of the ancient thunder dragon, Naezith! 
+					With the powers granted to him by the dragon, Kayra will destroy the Remnants of Naezith, enchanted fragments of the dragon's body, 
+					to prevent anyone from exploiting their power for evil.</p>
+					
+					<hr>
+					<h3 id="history">History</h3>
+					<p>Tolga is a <?php echo floor((time() - strtotime('1995-05-15')) / 31556926); ?> years old 
+					student at Yildiz Technical University, department of Computer Engineering. Coding quickly became a passion for him and
+					he didn't wait for the university to get into the interesting lessons so he started learning stuff himself on the internet.</p>
+					
+					<p>He started developing this game 4th of August, 2014. Spending most of his time on the project, he enjoys it a lot and 
+					it feels more like a hobby rather than a work for him. As a competitive FPS (Overwatch/Quake/TF2) player, he've always loved playing
+					fast-paced games which need sharp skills and quick reaction times.</p>
+					
+					<p>After the game was shown to his gamer friends, a small closed community started growing with people who were interested at making the game a reality.</p>
+					
+					<p>While the game was still in early stages, Tolga took it to the Gaming Istanbul 2016 and people were very interested in it.
+					A friend posted the gameplay teaser to the <a href="https://www.reddit.com/r/gaming/comments/453epx/one_of_my_friends_has_been_making_a_game_for_the/">Reddit</a> 
+					and it stayed at the "gaming" subreddit front page for 24 hours and reached 36K views on the shared YouTube video.</p>
+					
+					<p>Game also got help by epic people such as <a href="https://twitter.com/epicvoiceguy">Jon Bailey (Honest Trailers)</a> who narrated the storyboard, and 
+					<a href="https://www.artstation.com/artist/mertun">Ömer Tunç</a> who drew two beautiful artworks of the dragon Naezith.</p>
+					
+					<p>Remnants of Naezith entered Steam Greenlight at 29.01.2017 and got Greenlit in just a single day! In the same week, Tolga took the game to Gaming Istanbul 2017 where it
+					gained big praise and attention from the visitors and the press.</p>
+					
+					<p>At the Gaming Istanbul 2017, Remnants of Naezith won the indie prize which is an indie booth at the QUO VADIS event in Berlin. Tolga took the game there and has been chosen as one of 10 indie games which will do a presentation at the main stage. After the presentations, Remnants of Naezith got the 3rd place in the Best of QUO VADIS 2017.</p>
+					
+					<p>Remnants of Naezith also got covered on <a href="http://kotaku.com/remnants-of-naezith-shows-gaming-needs-more-grappling-h-1795610852">Kotaku</a>, <a href="http://www.canardpc.com/canard-pc-355-completement-a-mass.html">Canard PC(355, pg 56)</a>, <a href="http://jp.automaton.am/articles/newsjp/20170130-39303/">AUTOMATON</a> and <a href="http://oyungezer.com.tr/forum/discussion/1651186/oyungezer-113-mart-2017">Oyungezer(113, pg 36)</a>.</p>
+					
+					<p>It keeps growing more and more with the small but awesome closed community it has. Graphics are still being drawn and levels are still being polished.</p>
+					
+					<hr>
+					<h3 id="features">Features</h3>
+					<ul>
+						<li>Level selection
+							<ul>
+								<li>Proceed through four chapters with 20 branching levels in each.</li>
+							</ul>
+						</li>
+						
+						<li>Secret levels
+							<ul>
+								<li>Find the hidden remnants to unlock secret levels.</li>
+							</ul>
+						</li>
+						
+						<li>Leaderboards
+							<ul>
+								<li>Watch top player replays and improve your skills to ascend.</li>
+							</ul>
+						</li>
+						
+						<li>Global rankings
+							<ul>
+								<li>Get a badge depending on your overall performance and compete worldwide.</li>
+							</ul>
+						</li>
+						
+						<li>Ghosts
+							<ul>
+								<li>Race against other people's best times or your own.</li>
+							</ul>
+						</li>
+						
+						<li>Speedrun mode
+							<ul>
+								<li>Try going through the entire game or a specific chapter as fast as possible.</li>
+							</ul>
+						</li>
+						
+						<li>Custom levels
+							<ul>
+								<li>Explore the countless levels made by the community and design your own with an easy to use in-game level editor.</li>
+							</ul>
+						</li>
+						
+						<li>Gamepad support
+							<ul>
+								<li>Have a smooth gameplay experience with your controller.</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+					
+				<div class="kit_content">
+					<hr>
+					<h3 id="videos">Videos</h3>
+					<figure class="yt">
+						<iframe src="https://www.youtube.com/embed/dCLqLxzm79U" allowfullscreen></iframe>
+					</figure>
+					<a class="extlink" href="https://www.dropbox.com/s/tsaodpbaxsv2q9z/greenlight_trailer.mp4?raw=1">Download the raw .mp4</a>
+					
+					<hr>
+					<h3 id="images">Images</h3>
+					<a href="https://www.dropbox.com/sh/kb2i1ve1aim4v5m/AAB5_3faQ75FTPa6yIdYBAIga/screenshots/1.png?raw=1" target="_blank"><img src="http://www.dropbox.com/sh/kb2i1ve1aim4v5m/AAB5_3faQ75FTPa6yIdYBAIga/screenshots/1.png?raw=1" alt="Screenshot"></a>
+					<a href="https://www.dropbox.com/sh/kb2i1ve1aim4v5m/AADLKh-km8zKSFoq1qUqd2D8a/screenshots/2.png?raw=1" target="_blank"><img src="http://www.dropbox.com/sh/kb2i1ve1aim4v5m/AADLKh-km8zKSFoq1qUqd2D8a/screenshots/2.png?raw=1" alt="Screenshot"></a>
+					<a href="https://www.dropbox.com/sh/kb2i1ve1aim4v5m/AAB_yMMz5Nd4rHoDkod6kQuca/screenshots/3.png?raw=1" target="_blank"><img src="http://www.dropbox.com/sh/kb2i1ve1aim4v5m/AAB_yMMz5Nd4rHoDkod6kQuca/screenshots/3.png?raw=1" alt="Screenshot"></a>
+					<a href="https://www.dropbox.com/sh/kb2i1ve1aim4v5m/AABzHPuVgVthqyniiEU41zzHa/screenshots/4.png?raw=1" target="_blank"><img src="http://www.dropbox.com/sh/kb2i1ve1aim4v5m/AABzHPuVgVthqyniiEU41zzHa/screenshots/4.png?raw=1" alt="Screenshot"></a>
+					<a href="https://www.dropbox.com/sh/kb2i1ve1aim4v5m/AADIVxSuRSSrsvvx6_nPNEFAa/screenshots/5.png?raw=1" target="_blank"><img src="http://www.dropbox.com/sh/kb2i1ve1aim4v5m/AADIVxSuRSSrsvvx6_nPNEFAa/screenshots/5.png?raw=1" alt="Screenshot"></a>
+					<a href="https://www.dropbox.com/sh/kb2i1ve1aim4v5m/AACdsJz0Cr10eJWv81_NTfNBa/screenshots/6.png?raw=1" target="_blank"><img src="http://www.dropbox.com/sh/kb2i1ve1aim4v5m/AACdsJz0Cr10eJWv81_NTfNBa/screenshots/6.png?raw=1" alt="Screenshot"></a>
+					<a class="extlink" href="https://www.dropbox.com/sh/jj4vu4gzlhao9c9/AAA6Qt9yUJOfJSX8oAVcCRcSa?raw=1">Download more screenshots & artwork as a .zip</a>
+
+					<hr>
+					<h3 id="logo">Logo & Icons</h3>
+					<a href="https://www.dropbox.com/s/n285jauvpq9xelb/title.png?raw=1" target="_blank"><img src="https://www.dropbox.com/s/n285jauvpq9xelb/title.png?raw=1" alt="logo"></a>
+					<a href="https://www.dropbox.com/s/gxaknr7pal6adpq/feature_image.png?raw=1" target="_blank"><img src="https://www.dropbox.com/s/gxaknr7pal6adpq/feature_image.png?raw=1" alt="logo"></a>
+					<a href="https://www.dropbox.com/s/fs8hlxf19b9senr/greenlight_logo.gif?raw=1" target="_blank"><img src="https://www.dropbox.com/s/fs8hlxf19b9senr/greenlight_logo.gif?raw=1" alt="logo"></a>
+					<a href="https://www.dropbox.com/s/1jhkqg21ekgxsyx/key_image.png?raw=1" target="_blank"><img src="https://www.dropbox.com/s/1jhkqg21ekgxsyx/key_image.png?raw=1" alt="logo"></a>
+					<a class="extlink" href="https://www.dropbox.com/sh/89fo22imbwqja88/AABWckKmTX0Lk90v3vhDqPpUa?raw=1">Download more logo & icon assets as a .zip</a>
+				
+					<hr>
+					<div class="kit_footer">
+						<h3 id="credits">Credits</h3>
+						<ul class="nested">
+							<li><a href="http://naezith.com/?id=contact">Tolga Ay</a>
+								<ul>
+									<li>Lead, coding, game design</li>
+								</ul>
+							</li>
+							
+							<li><a href="http://decldev.kapsi.fi/">decldev</a>
+								<ul>
+									<li>Level design, webmaster</li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="https://twitter.com/Mugikala">SiikiKala</a>, 
+								<a href="https://www.artstation.com/artist/sapdash">Darya Kojemyakina</a>, 
+								<a href="https://www.artstation.com/artist/makskneht">Maks Kneht</a>, 
+								<a href="https://www.artstation.com/artist/erek">Erek Rabang</a>
+								<ul>
+									<li>Graphics, artwork</li>
+								</ul>
+							</li>
+							
+							<li><a href="https://soundcloud.com/pronomicalartist">PronomicalArtist</a>
+								<ul>
+									<li>Music</li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="https://github.com/Yengas">Yengas</a>, 
+								<a href="http://mehmetoguzderin.com/">Mehmet Oğuz Derin</a>, 
+								<a href="https://www.linkedin.com/in/kevinzhang711">Kevin Zhang</a>, 
+								<a href="http://www.sfml-dev.org/">SFML IRC Community</a>
+								<ul>
+									<li>Technical support</li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="http://steamcommunity.com/id/deltis/">deltis</a>, 
+								<a href="http://steamcommunity.com/profiles/76561197963807395/">Wili</a> 
+								<ul>
+									<li>Mapping, playtesting</li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="http://steamcommunity.com/profiles/76561198087418775">Mijzical</a>, 
+								<a href="http://steamcommunity.com/profiles/76561198047120857">Luffy</a> 
+								<ul>
+									<li>Intense bug search, playtesting</li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="http://steamcommunity.com/id/Boneslark/">Boneslark</a>, 
+								<a href="https://twitter.com/teraka91">Teraka</a>, 
+								<a href="http://steamcommunity.com/id/flamingbeard">fireball</a>, 
+								<a href="https://twitter.com/wwwwvwwwvwvwwwv">walter</a>, 
+								<a href="https://twitter.com/MunchaKoopasSK">MunchaKoopas</a>
+								<ul>
+									<li>Game design advices</li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="http://www.canuzer.com/">Can Uzer</a>
+								<ul>
+									<li>Additional sound design help</li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="https://twitter.com/epicvoiceguy">Jon Bailey</a>, 
+								<a href="https://www.artstation.com/artist/mertun">Ömer Tunç</a>, 
+								<a href="http://dustforce.com/community">Dustforce community</a>
+								<ul>
+									<li>Special thanks</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					
+					<div class="kit_footer">
+						<h3 id="contact">Contact us</h3>
+						<ul class="nested">
+							<li>Inquiries
+								<ul>
+									<li><a href="mailto:tolgaay@live.com">tolgaay@live.com</a></li>
+								</ul>
+							</li>
+							
+							<li>Social
+								<ul>
+									<li><a href="http://twitter.com/naezith">Twitter</a></li>
+									<li><a href="http://facebook.com/naezith">Facebook</a></li>
+									<li><a href="https://www.youtube.com/naezith">YouTube</a></li>
+								</ul>
+							</li>
+							
+							<li>Steam
+								<ul>
+									<li><a href="http://steamcommunity.com/id/naezith/">Tolga Ay</a></li>
+									<li><a href="http://steamcommunity.com/groups/naezith">Steam Group</a></li>
+									<li><a href="http://steamcommunity.com/sharedfiles/filedetails/?id=765959054">Greenlight</a></li>
+								</ul>
+							</li>
+							
+							<li>Web
+								<ul>
+									<li><a href="http://naezith.com">naezith.com</a></li>
+									<li><a href="https://www.reddit.com/r/naezith">Subreddit</a></li>
+									<li><a href="https://forums.tigsource.com/index.php?topic=50838.0">Devlog</a></li>
+									<li><a href="https://soundcloud.com/pronomicalartist/sets/remnants-of-naezith">OST</a></li>
+									<li><a href="http://www.indiedb.com/games/naezith/">IndieDB</a></li>
+									<li><a href="http://naezith.com/?id=contact">Tolga Ay</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
---
+++ index.php
@@ -0,0 +1,64 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Remnants of Naezith</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="icon" type="image/x-icon" href="_img/k.ico">
+	<meta name="viewport" content="width=device-width">
+	<meta charset="utf-8">
+	<meta name="description" content="A fast-paced platformer which has the grappling hook in it's core!">
+</head>
+
+<?php
+	$pgs = array(
+	'home'=>'home.php',
+	'gallery'=>'gallery.php',
+	'contact'=>'contact.php',
+	'presskit'=>'presskit.php',
+	);
+	$s = (!empty($_GET['id']))?$_GET['id']:'home'; 
+?>
+
+<body>
+	<div id="banner"></div>
+
+	<header>
+		<a href="."><img src="_img/title.png" alt="Remnants of Naezith"></a>
+		<span class="po"></span>
+	</header>
+
+	<div id="wrap">
+		<nav>
+			<div>
+				<?php if ($s == "home") echo '<span class="home">home</span>'."\n"; else echo '<a class="home" href=".">home</a>'."\n";?>
+				<a href="https://forums.tigsource.com/index.php?topic=50838.0" target="_blank"><img src="_img/d.png" alt="">devlog</a>
+				<a href="https://twitter.com/naezith" target="_blank" ><img src="_img/t.png" alt="">twitter</a>
+				<a href="http://facebook.com/naezith" target="_blank"><img src="_img/f.png" alt="">facebook</a>
+			</div>
+			<div>
+				<a href="https://soundcloud.com/pronomicalartist/sets/remnants-of-naezith" target="_blank"><img src="_img/s.png" alt="">soundcloud</a>
+				<a href="https://www.reddit.com/r/naezith" target="_blank"><img src="_img/r.png" alt="">reddit</a>
+				<?php if ($s == "gallery") echo '<span class="page">gallery</span>'."\n"; else echo '<a class="page" href="?id=gallery">gallery</a>'."\n";?>
+				<?php if ($s == "contact") echo '<span class="page">contact</span>'."\n"; else echo '<a class="page" href="?id=contact">contact</a>'."\n";?>
+			</div>
+		</nav>
+
+		<main>
+<?php
+				if (isset($pgs[$s]) && file_exists ("_p/".$pgs[$s])) {
+					include_once ("_p/".$pgs[$s]);
+				}
+				echo "\n";
+?>
+		</main>
+	</div>
+		
+	<footer>
+		<a href="?id=presskit">Presskit</a> <span>&copy; Tolga Ay 2017</span>
+		<a href="http://www.indiedb.com/games/naezith" title="View Remnants of Naezith on Indie DB" target="_blank"><img src="http://button.indiedb.com/popularity/medium/games/49414.png" alt="Remnants of Naezith"></a>
+		<span title="Also indie ^-^">website by decldev</span>
+		<span class="pb"></span>
+	</footer>
+</body>
+
+</html>
